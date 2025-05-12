fe5076101944f9d6170276e9d88df1a89eda8712
josias76 <josias76nteme@gmail.com>
2025-05-12 18:36:43 +0200

    add copyright

--- Appsantépublique.py
+++ Appsantépublique.py
@@ -157,3 +157,14 @@ if uploaded_files:
     # Ici tu peux continuer avec ton code de visualisation
 else:
     st.info("Veuillez importer au moins un fichier CSV pour afficher le tableau de bord.")
+
+
+# ---------- Bandeau de bas de page ----------
+st.markdown("""
+    <hr style="border-top: 1px solid #4CAF50; margin-top: 50px;"/>
+    <div style="text-align: center; color: #888; font-size: 0.9em;">
+        &copy; 2025 <strong>Josias Nteme</strong> - Tous droits réservés.
+    </div>
+""", unsafe_allow_html=True)
+
+
